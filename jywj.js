b28be4c55970770d2f907970632716f38cb7a7ca
Aurora72 <43649019+Aurora72@users.noreply.github.com>
2018-10-06 00:46:32 +0800

    Delete jywj.js

--- jywj.js
+++
@@ -1,9 +0,0 @@
-window.func=function(lib,game,ui,get,ai,_status){
-	if(lib.program!=undefined){
-		for(var i in lib.program.character){
-			if(lib.config['extension_扩展ol_'+i]==true){
-				lib.config.characters.remove(i)
-			};
-		};
-	};
-}
